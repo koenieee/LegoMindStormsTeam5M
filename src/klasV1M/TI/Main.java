6d184be2af99300c85a0312588cb5fc52987fd41
Koen van der Kruk <koenvdkruk@gmail.com>
2015-03-19 12:53:56 +0100

    added main and package

---
+++ src/klasV1M/TI/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package klasV1M.TI;
+
+/**
+ * @author koen
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
